928368b9afdd7dab3ffba3b1719cc9fc7af9756f
Lakshay Sharma <56682042+LAKSHAYNE@users.noreply.github.com>
2024-05-19 22:03:19 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.get('/ping',(req,res)=>res.send('me is here'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
