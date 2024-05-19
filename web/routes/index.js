4989557db4b46e4a7b276646c46c606242c28da4
Lakshay Sharma <56682042+LAKSHAYNE@users.noreply.github.com>
2024-05-19 22:37:54 +0530

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 291,
+      count: 2912,
       shape: 'rectangle'
     })
 })
